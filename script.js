e07ad1357f5f743594148068a79d17bb209dca28
Deni Booe <102338302+vistaOS404@users.noreply.github.com>
2025-02-09 00:57:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.add("loaded");
+});
+
+
 document.addEventListener("mousemove", function (e) {
     const cursor = document.getElementById("custom-cursor");
     if (cursor) {  // Prevent errors if the cursor is missing
